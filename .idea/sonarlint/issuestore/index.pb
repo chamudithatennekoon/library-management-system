d7b05a8a24d2e87af93fca4ef93003a510cc6d7a
Chamuditha Tennekoon <chamudithabhagya@gmail.com>
2024-02-13 19:36:22 +0530

    connect to db

--- .idea/sonarlint/issuestore/index.pb
+++ .idea/sonarlint/issuestore/index.pb
@@ -15,3 +15,7 @@ h
 8src/main/java/edu/icet/service/impl/BookServiceImpl.java,6\d\6dfe4c0f8d590de2dd8d438d00db7885fca8eeee
 _
 /src/main/java/edu/icet/service/BookService.java,6\6\66066eb8f490dd57ce7148aadee471c3c299aff0
+e
+5src/main/java/edu/icet/repository/BookRepository.java,a\6\a61fa4d75f4cd04529468ef8b83e4b3b897ce647
+]
+-src/main/java/edu/icet/entity/BookEntity.java,8\8\886d497d4a297116fd775c3fa14aed82dd8a146f
--- src/main/java/edu/icet/service/impl/BookServiceImpl.java
+++ src/main/java/edu/icet/service/impl/BookServiceImpl.java
@@ -1,13 +1,28 @@
 package edu.icet.service.impl;
 
 import edu.icet.dto.Book;
+import edu.icet.entity.BookEntity;
+import edu.icet.repository.BookRepository;
 import edu.icet.service.BookService;
+import org.modelmapper.ModelMapper;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.context.annotation.Bean;
 import org.springframework.stereotype.Service;
 
 @Service
 public class BookServiceImpl implements BookService {
+
+    @Autowired
+    BookRepository repository;
+
+    ModelMapper mapper;
+    @Bean
+    public void setup(){
+        this.mapper=new ModelMapper();
+    }
     @Override
     public void addBook(Book book) {
-
+       BookEntity entity = mapper.map(book, BookEntity.class);
+       repository.save(entity);
     }
 }
