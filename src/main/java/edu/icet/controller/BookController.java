ec815f436f4061d280058cec75241110c4ff6aed
Chamuditha Tennekoon <chamudithabhagya@gmail.com>
2024-02-13 17:47:04 +0530

    add book method completed

--- src/main/java/edu/icet/controller/BookController.java
+++ src/main/java/edu/icet/controller/BookController.java
@@ -3,6 +3,8 @@ package edu.icet.controller;
 import edu.icet.dto.Book;
 import edu.icet.service.BookService;
 import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.web.bind.annotation.PostMapping;
+import org.springframework.web.bind.annotation.RequestBody;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
@@ -12,7 +14,8 @@ public class BookController {
 
     @Autowired
     BookService service;
-    public void addBook(Book book){
-
+    @PostMapping
+    public void addBook(@RequestBody Book book){
+        service.addBook(book);
     }
 }
