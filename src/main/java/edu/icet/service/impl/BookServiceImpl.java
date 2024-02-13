9945bafa617b7ffca70d1b891de6604c2b2f8772
Chamuditha Tennekoon <chamudithabhagya@gmail.com>
2024-02-13 18:26:47 +0530

    create yml file

--- src/main/java/edu/icet/service/impl/BookServiceImpl.java
+++ src/main/java/edu/icet/service/impl/BookServiceImpl.java
@@ -2,7 +2,9 @@ package edu.icet.service.impl;
 
 import edu.icet.dto.Book;
 import edu.icet.service.BookService;
+import org.springframework.stereotype.Service;
 
+@Service
 public class BookServiceImpl implements BookService {
     @Override
     public void addBook(Book book) {
